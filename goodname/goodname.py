052c7ee0ce13e0b5ec058077d93f396915f8f316
hexj <xiaojiao.he@tongdun.cn>
2019-08-19 14:41:36 +0800

    add data

--- goodname/goodname.py
+++ goodname/goodname.py
@@ -26,6 +26,7 @@ def main():
     g = os.walk(r"data")
     for path,dir_list,file_list in g:  
         for file_name in file_list:  
+            # print(file_name)
             print(os.path.join(path, file_name) )
 
     pass
